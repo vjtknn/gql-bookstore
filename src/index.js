87aeff2aebed64a1eeb3dc2db28b4fc3169c0372
Wojciech Winnicki <wbwinnicki@gmail.com>
2020-02-11 19:35:23 +0100

    Add Author schema, add author to book, add Status enum

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ const { ApolloServer, gql } = require("apollo-server");
 const { books } = require("./data");
 
 const typeDefs = gql`
+  enum Status {
+    READ
+    WATCH_LIST
+    NOT_INTERESTED
+    UNKNOWN
+  }
+
   type Book {
+    id: ID!
     title: String!
-    author: String!
+    author: Author
     pages: Int
     publishDate: String
     publisher: String
+    status: Status
+  }
+
+  type Author {
+    id: ID!
+    first_name: String!
+    last_name: String!
+    dateOfBirth: String
   }
 
   type Query {
