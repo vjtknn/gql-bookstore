1a37bf420538a86eb9b61e5dfbb8c2beb2040caf
Wojciech Winnicki <wojciech.winnicki@gmail.com>
2020-02-11 18:46:12 +0100

    Sample data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const books = [
+  {
+    title: "Fundation",
+    author: "Isaac Asimov",
+    pages: 255,
+    publishDate: "21-03-1951",
+    publisher: "Gnome press"
+  },
+  {
+    title: "Fundation and Empire",
+    author: "Isaac Asimov",
+    pages: 247,
+    publishDate: "21-03-1952",
+    publisher: "Gnome press"
+  }
+];
+
+module.exports = {
+  books
+};
