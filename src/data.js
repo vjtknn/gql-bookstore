37e5a9a9f60fd98b877efccca77f9c5b7af9331a
Wojciech Winnicki <wbwinnicki@gmail.com>
2020-02-11 19:35:41 +0100

    Add author object to book

--- src/data.js
+++ src/data.js
@@ -1,14 +1,20 @@
 const books = [
   {
     title: "Fundation",
-    author: "Isaac Asimov",
+    author: {
+      first_name: "Isaac",
+      last_name: "Asimov"
+    },
     pages: 255,
     publishDate: "21-03-1951",
     publisher: "Gnome press"
   },
   {
     title: "Fundation and Empire",
-    author: "Isaac Asimov",
+    author: {
+      first_name: "Isaac",
+      last_name: "Asimov"
+    },
     pages: 247,
     publishDate: "21-03-1952",
     publisher: "Gnome press"
